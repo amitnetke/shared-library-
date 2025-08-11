7cbebf1a05dd9ca51dbf519835a80abe56e4a60f
amit <amit.netke@ymail.com>
2025-08-11 19:55:57 +0530

    add

--- vars/mavenBuild.groovy
+++ vars/mavenBuild.groovy
@@ -1,3 +1,8 @@
 def call(String branch) {
-    checkout scmGit(branches: [[name: '*/master']], extensions: [], userRemoteConfigs: [[credentialsId: 'c2003d50-5f59-4be9-a918-c6c6be215279', url: 'https://github.com/amitnetke/test.git']])
+
+    checkout 
+        scmGit(
+            branches: [[name: stageParams.branch]], 
+            extensions: [], 
+            userRemoteConfigs: [[credentialsId: 'c2003d50-5f59-4be9-a918-c6c6be215279', url: stageParams.url ]])
 }
